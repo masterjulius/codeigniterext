76ea03fd2c977441429b273de34f6e068334c235
masterjulius <opensourcecagayan@gmail.com>
2017-05-18 14:48:46 +0800

    fixed the exports methods in the exports library by making the options dynamic.

--- application/libraries/Exports.php
+++ application/libraries/Exports.php
@@ -7,15 +7,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * An open source application development framework for PHP 5.1.6 or newer
  *
  * @package     CodeIgniter
+ * @uses     	PHPExcel Library
+ * @link     	https://github.com/PHPOffice/PHPExcel
  * @author      Julius Palcong
  * @copyright   Copyright (c) 2017, Cagayan Provincial Capitol.
  * @license     
- * @link        http://www.cagayan.gov.ph
+ * @link        https://github.com/masterjulius/codeigniterlibrary
  * @since       Version 1.0
  * @filesource
  */
 
 class Exports {
+
+	/** Local Variable Declarations */
 
 	public function excel( $data, $attributes = array(
 			'sheetIndex' 			=> 0,
@@ -28,52 +32,28 @@ class Exports {
 			'dataRowStart'			=> 2,
 			'fileNamePrefix'		=> 'Export_',
 			'dateFormat'			=> 'Y-m-d_H-i-s', 
-			'passwordProtected'		=> true,
+			'passwordProtected'		=> false,
 			'workbookPassword'		=> '1234567890',
 			'sheetPassword'			=> '1234567890'
 		) 
 	) {
 
+		// Convert the array to object if it is array
 		$attributes = is_array( $attributes ) ? (object) $attributes : $attributes;	
 
+		foreach ( $data as $key => $value ) {
 
-		// echo "<pre>";
-		// 	print_r($data);
-		// echo "</pre>";
-		// echo "<hr/>";
-
-		// echo "<pre>";
-		// 	print_r($attributes);
-		// echo "</pre>";
-		// echo "<hr/>";
-
-		foreach ($data as $key => $value) {
-
-			if ( is_object($data[$key]) ) {
+			if ( is_array( $data[$key] ) ) {
 				
 				// variable is object
-
-			} else {
-				
-				// variable is array
+				$data[$key] = (object) $data[$key];
 
 			}
 
 		}
 
-		echo $attributes->sheetIndex;
-
-		// print_r($attributes['cellValueKeys'][0]);
-
-		// foreach ($data as $row) {
-		// 	$key = $attributes['cellValueKeys'][1];
-		// 	echo $row->$key . "<br/>";
-		// }
-
-		// ------------------------------------------------------------------------------------------
-
-		require (APPPATH . 'third_party/Classes/PHPExcel.php');
-		require (APPPATH . 'third_party/Classes/PHPExcel/Writer/Excel2007.php');
+		require ( APPPATH . 'third_party/Classes/PHPExcel.php' );
+		require ( APPPATH . 'third_party/Classes/PHPExcel/Writer/Excel2007.php' );
 
 		$objPHPExcel = new PHPExcel();
 		$objPHPExcel->getProperties()->setCreator();
@@ -81,58 +61,74 @@ class Exports {
 		$objPHPExcel->getProperties()->setSubject();
 		$objPHPExcel->getProperties()->setDescription();
 
+		// set active sheet
 		$objPHPExcel->setActiveSheetIndex( $attributes->sheetIndex );
 
-		// $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'ITEM ID');
-		// $objPHPExcel->getActiveSheet()->SetCellValue('B1', 'ITEM CODE');
-		// $objPHPExcel->getActiveSheet()->SetCellValue('C1', 'ITEM NAME');
-		// $objPHPExcel->getActiveSheet()->SetCellValue('D1', 'ITEM DESCIPRTION');
-		// $objPHPExcel->getActiveSheet()->SetCellValue('E1', 'ITEM DATE CREATED');
+		$headerColumnArr = $attributes->headerColumns;
 
-		// $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'ITEM ID');
-		// $objPHPExcel->getActiveSheet()->SetCellValue('B1', 'ITEM CODE');
-		// $objPHPExcel->getActiveSheet()->SetCellValue('C1', 'ITEM NAME');
-		// $objPHPExcel->getActiveSheet()->SetCellValue('D1', 'ITEM DESCIPRTION');
-		// $objPHPExcel->getActiveSheet()->SetCellValue('E1', 'ITEM DATE CREATED');
+		$headerColumnStart = $attributes->headerColumnStart;
+		$headerRowStart = $attributes->headerRowStart;
+		for ( $i = 0; $i < count( $headerColumnArr ); $i++ ) {
+			
+			$cellLocation = $headerColumnStart . $headerRowStart;
 
-		// // start with row number 2(Not in array type that starts with the number 0)
-		// $rowIndex = 2;
+			$objPHPExcel->getActiveSheet()->SetCellValue( $cellLocation, $headerColumnArr[$i] );
 
-		// foreach ($data as $row) {
+			$headerColumnStart++;
 
-		// 	$objPHPExcel->getActiveSheet()->SetCellValue('A'.$rowIndex, $row->id);
-		// 	$objPHPExcel->getActiveSheet()->SetCellValue('B'.$rowIndex, $row->code);
-		// 	$objPHPExcel->getActiveSheet()->SetCellValue('C'.$rowIndex, $row->name);
-		// 	$objPHPExcel->getActiveSheet()->SetCellValue('D'.$rowIndex, $row->description);
-		// 	$objPHPExcel->getActiveSheet()->SetCellValue('E'.$rowIndex, date( "Y-m-d h:i:s A", strtotime($row->date) ) );
+		}
 
-		// 	$rowIndex++;
-		// }
+		// start with row number 2(Not in array type that starts with the number 0)
+		$cellValueKeyArrs = $attributes->cellValueKeys;
 
-		// $fileName = "Consigment-Export-" . date('Y-m-d-H-i-s') . ".xlsx";
+		$dataRowIndex = $attributes->dataRowStart;
 
-		// $objPHPExcel->getActiveSheet()->setTitle("Exported Items");
+		foreach ( $data as $row ) {
 
-		// // Set password against the spreadsheet file
-		// $objPHPExcel->getSecurity()->setLockWindows(true);
-		// $objPHPExcel->getSecurity()->setLockStructure(true);
-		// $objPHPExcel->getSecurity()->setWorkbookPassword('123');
+			$dataColumnStart = $attributes->dataColumnStart;
 
-		// $objPHPExcel->getActiveSheet()->getProtection()->setSheet(true);
-		// $objPHPExcel->getActiveSheet()->getProtection()->setSort(true);
-		// $objPHPExcel->getActiveSheet()->getProtection()->setInsertRows(true);
-		// $objPHPExcel->getActiveSheet()->getProtection()->setFormatCells(true);
+			for ( $i = 0; $i < count( $cellValueKeyArrs ); $i++ ) {
 
-		// $objPHPExcel->getActiveSheet()->getProtection()->setPassword('123');
+				$cellLocation = $dataColumnStart . $dataRowIndex;
+				$key = $cellValueKeyArrs[$i];
 
-		// header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-		// header('Content-Disposition: attachment;filename="' . $fileName . '"');
-		// header('Cache-Control: max-age=0');
+				$objPHPExcel->getActiveSheet()->SetCellValue( $cellLocation, $row->$key );
 
-		// $writer = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-		// ob_end_clean();
-		// $writer->save('php://output');
-		// exit;
+				$dataColumnStart++;
+
+			}
+
+			$dataRowIndex++;
+
+		}
+
+		$fileName = $attributes->fileNamePrefix . date( $attributes->dateFormat ) . ".xlsx";
+
+		$objPHPExcel->getActiveSheet()->setTitle( $attributes->sheetTitle );
+
+		// Set password against the spreadsheet file
+		if ( $attributes->passwordProtected === true ) {
+
+			$objPHPExcel->getSecurity()->setLockWindows( true );
+			$objPHPExcel->getSecurity()->setLockStructure( true );
+			$objPHPExcel->getSecurity()->setWorkbookPassword( $attributes->workbookPassword );
+
+			$objPHPExcel->getActiveSheet()->getProtection()->setSheet( true );
+			$objPHPExcel->getActiveSheet()->getProtection()->setSort( true );
+			$objPHPExcel->getActiveSheet()->getProtection()->setInsertRows( true );
+			$objPHPExcel->getActiveSheet()->getProtection()->setFormatCells( true );
+			$objPHPExcel->getActiveSheet()->getProtection()->setPassword( $attributes->sheetPassword );
+
+		}
+
+		header( 'Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' );
+		header( 'Content-Disposition: attachment;filename="' . $fileName . '"' );
+		header( 'Cache-Control: max-age=0' );
+
+		$writer = PHPExcel_IOFactory::createWriter( $objPHPExcel, 'Excel2007' );
+		ob_end_clean();
+		$writer->save( 'php://output' );
+		exit;
 
 	}
 
